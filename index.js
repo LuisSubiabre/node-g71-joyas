31dc97f823a9b019f2a079ff685632fa868f54ed
Luis Subiabre <luis.subiabre@gmail.com>
2024-12-14 00:00:55 -0300

    morgan middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import morgan from "morgan";
 
 import joyasRoute from "./routes/joyasRoute.js";
 
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+//middleware "morgan" para generar informes o reportes de alguna actividad o evento especíco que ocurra en cada una de las rutas.
+app.use(morgan("dev"));
 
 app.listen(PORT, () => {
   console.log(`Servidor lanzado 🚀 en: http://localhost:${PORT} `);
